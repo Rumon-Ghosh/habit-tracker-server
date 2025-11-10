2af56a68e792a53416c3bae3b9d78360abad3648
Rumon-Ghosh <rumon698@gmail.com>
2025-11-10 16:58:47 +0600

    added the delete api

--- index.js
+++ index.js
@@ -80,6 +80,7 @@ async function run() {
       res.send(result)
     })
 
+    // delete api
     app.delete('/habits/:id', async (req, res) => {
       const id = req.params.id;
       const filter = { _id: new ObjectId(id) };
