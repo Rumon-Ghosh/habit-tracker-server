f1ecedcb029a7ffd16d50f8a30538462fcd0685c
Rumon-Ghosh <rumon698@gmail.com>
2025-11-12 00:14:29 +0600

    deployed in vercel

--- index.js
+++ index.js
@@ -6,7 +6,9 @@ const app = express()
 const port = process.env.PORT || 3000
 
 
-const serviceAccount = require("./habit-tracker-firebase-config.json");
+// index.js
+const decoded = Buffer.from(process.env.FIREBASE_SERVICE_KEY, "base64").toString("utf8");
+const serviceAccount = JSON.parse(decoded);
 
 admin.initializeApp({
   credential: admin.credential.cert(serviceAccount)
@@ -144,8 +146,8 @@ async function run() {
 
 
 
-    await client.connect();
-    await client.db("admin").command({ ping: 1 });
+    // await client.connect();
+    // await client.db("admin").command({ ping: 1 });
     console.log("Pinged your deployment. You successfully connected to MongoDB!");
   } finally {
     // Ensures that the client will close when you finish/error
